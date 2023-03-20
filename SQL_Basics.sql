f0f6960aeb816e7d66ba101c01da0d780fb8c26f
Rutuja Hole <47391313+Rutujahole@users.noreply.github.com>
2023-03-20 23:31:43 +0530

    Add files via upload

---
+++ SQL_Basics.sql
@@ -0,0 +1,16 @@
+/*CREATE DATABASE shirts_db;
+
+--USE shirts_db;
+
+/CREATE TABLE shirts
+(
+ --  shirt_id INT AUTO_INCREMENT PRIMARY KEY,
+  -- article VARCHAR(50),
+ --  color VARCHAR(50),
+ --  shirts_size VARCHAR(5),
+ --  last_worn int
+);*/
+
+DESC shirts;
+
+
