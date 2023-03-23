046844f26737be3955995c993edef11b93f1c1bd
Rutuja Hole <47391313+Rutujahole@users.noreply.github.com>
2023-03-23 18:39:44 +0530

    Update SQL_Refining_Selections.sql

--- SQL_Refining_Selections.sql
+++ SQL_Refining_Selections.sql
@@ -92,4 +92,60 @@ FROM
 ORDER BY title
 LIMIT 14 , 877437;
 
+/*exercises*
+/* select all story collections titles that contain stories*/
+SELECT 
+    *
+FROM
+    books;
+
+SELECT 
+    title
+FROM
+    books
+WHERE
+    title LIKE '%stories%';
+
+/* Find the longest book - print out title and page count*/
+SELECT 
+    title, pages
+FROM
+    books
+ORDER BY pages DESC
+LIMIT 1;
+
+/*print a summary cntaining the title and year for 3 most recent books*/
+SELECT 
+    CONCAT(title, ' - ', released_year) AS summary
+FROM
+    books
+ORDER BY released_year DESC
+LIMIT 3;
+
+/*find the 3 books with the lowest stock select title , year and stock*/
+SELECT 
+    title, released_year, stock_quantity
+FROM
+    books
+ORDER BY stock_quantity , released_year DESC
+LIMIT 3;
+
+/* print title, author_lname, sorted first by  author_lname and then by title*/
+SELECT 
+    title, author_lname
+FROM
+    books
+ORDER BY author_lname , title;
+
+/* sort alphabetically by last name all in uppercase - MY FAVORITE AUTHPR IS RAYMOND CARVER ! as yell*/
+
+SELECT 
+    CONCAT('MY FAVORITE AUTHOR IS ',
+            UCASE(author_fname),
+            ' ',
+            UPPER(author_lname),
+            ' !') AS yell
+FROM
+    books
+ORDER BY author_lname;
 
